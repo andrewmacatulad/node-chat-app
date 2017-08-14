9469c49a17954a8c501448eb5ea6209459ddb6bf
Marc Andrew <andrewmacatulad@gmail.com>
2017-08-14 18:07:41 +0800

    Add autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 var socket = io();
 
+function scrollToBottom () {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child')
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+	
+}
+
+
 	socket.on('connect', () => {
 		console.log('Connect to the server')
 
@@ -24,12 +43,8 @@ var socket = io();
 		});
 
 		jQuery('#messages').append(html);
+		scrollToBottom();
 
-		// var li = jQuery('<li></li>')
-		
-		// li.text(`${message.from}: ${formattedTime}: ${message.text}`);
-
-		// jQuery('#messages').append(li);
 	})
 
 	socket.on('newLocationMessage', function (message) {
@@ -44,14 +59,8 @@ var socket = io();
 		});
 
 		jQuery('#messages').append(html);
+		scrollToBottom();
 
-		// var li = jQuery('<li></li>')
-		// var a = jQuery('<a target="_blank">My Current Location</a>')
-
-		// li.text(`${message.from}: ${formattedTime}: `);
-		// a.attr('href', message.url)
-		// li.append(a);
-		// jQuery('#messages').append(li);
 	})
 
 	// socket.emit('createMessage', {
