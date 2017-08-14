4e95447cb6a4c6f448cec5af53d950d0a02c1486
Marc Andrew <andrewmacatulad@gmail.com>
2017-08-14 17:04:13 +0800

    Add moustache

--- public/js/index.js
+++ public/js/index.js
@@ -14,22 +14,44 @@ var socket = io();
 	})
 
 	socket.on('newMessage', (message) => {
-		var li = jQuery('<li></li>')
 		var formattedTime = moment(message.createdAt).format('h:mm a')
-		li.text(`${message.from}: ${formattedTime}: ${message.text}`);
+		var template = jQuery('#message-template').html();
 
-		jQuery('#messages').append(li);
+		var html = Mustache.render(template, {
+			text: message.text,
+			from: message.from,
+			createdAt: formattedTime
+		});
+
+		jQuery('#messages').append(html);
+
+		// var li = jQuery('<li></li>')
+		
+		// li.text(`${message.from}: ${formattedTime}: ${message.text}`);
+
+		// jQuery('#messages').append(li);
 	})
 
 	socket.on('newLocationMessage', function (message) {
-		var li = jQuery('<li></li>')
+		
 		var formattedTime = moment(message.createdAt).format('h:mm a')
-		var a = jQuery('<a target="_blank">My Current Location</a>')
+		var template = jQuery('#location-message-template').html();
 
-		li.text(`${message.from}: ${formattedTime}: `);
-		a.attr('href', message.url)
-		li.append(a);
-		jQuery('#messages').append(li);
+		var html = Mustache.render(template, {
+			url: message.url,
+			from: message.from,
+			createdAt: formattedTime
+		});
+
+		jQuery('#messages').append(html);
+
+		// var li = jQuery('<li></li>')
+		// var a = jQuery('<a target="_blank">My Current Location</a>')
+
+		// li.text(`${message.from}: ${formattedTime}: `);
+		// a.attr('href', message.url)
+		// li.append(a);
+		// jQuery('#messages').append(li);
 	})
 
 	// socket.emit('createMessage', {
